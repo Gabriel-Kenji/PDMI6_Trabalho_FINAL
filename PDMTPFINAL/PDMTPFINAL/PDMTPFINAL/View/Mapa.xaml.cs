25a7698b7c4eb56431c3db1b48be328b5849995b
Gabriel Kenji Utiyama <68922914+Gabriel-Kenji@users.noreply.github.com>
2022-12-09 18:05:37 -0300

    .

---
+++ PDMTPFINAL/PDMTPFINAL/PDMTPFINAL/View/Mapa.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using Xamarin.Forms.Xaml;
+
+
+namespace PDMTPFINAL.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Mapa : ContentPage
+    {
+        public Mapa()
+        {
+            InitializeComponent();
+            LocalizacaoAsync();
+        }
+
+        async void LocalizacaoAsync()
+        {
+
+            var localizacao = await Geolocation.GetLastKnownLocationAsync();
+            Maps.MoveToRegion(MapSpan.FromCenterAndRadius(
+            new Position(localizacao.Latitude, localizacao.Longitude),
+            Distance.FromMiles(0.5)));
+
+        }
+    }
+}
